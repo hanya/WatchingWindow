6eee1d57b978e7b32d9696628a9e4ee376b8d983
Tsutomu Uchino <hanya.runo@gmail.com>
2013-03-01 21:17:28 +0900

    Fix wrong update URL

--- genres.py
+++ genres.py
@@ -40,7 +40,7 @@ xmlns:d="http://openoffice.org/extensions/description/2006">
 <d:OpenOffice.org-minimal-version value="3.4" d:name="OpenOffice.org 3.4" />
 </dependencies>
 <update-download>
-<src xlink:href="https://github.com/downloads/hanya/WatchingWindow/WatchingWindow-{VERSION}.oxt"/>
+<src xlink:href="https://raw.github.com/hanya/WatchingWindow/master/files/WatchingWindow-{VERSION}.oxt"/>
 </update-download>
 </description>
 """
